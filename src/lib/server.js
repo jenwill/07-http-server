d35b43a38202a91446ceca31a07bef5b3e0d08dc
Jennifer Williams <jennifer@squareandplum.com>
2018-04-24 17:57:17 -0700

    implement cowsay with query string and cowsay with random message

--- src/lib/server.js
+++ src/lib/server.js
@@ -4,6 +4,7 @@
 const http = require('http');
 const cowsay = require('cowsay');
 const bodyParser = require('./body-parser');
+const faker = require('faker');
 
 const server = module.exports = {};
 
@@ -19,15 +20,28 @@ const app = http.createServer((req, res) => {
         return undefined;
       }
       if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/') {
-        const cowsayText = cowsay.say({ text: 'sup.' });
-        const html = '<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cowsay">cowsay</a></li></ul></nav></header><main>This project is a very simple HTTP server by Jennifer Piper.</main></body></html>';
+        const html = `<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cowsay">cowsay</a></li></ul></nav></header><main><p>This project is a very simple HTTP server by Jennifer Piper.</p><p>To see a silly cow say your message, go to localhost:3000/cowsay?text=<em>your-message</em> with your browser.</p><p>To see the silly cow say a random message, go to localhost:3000/cowsay, or follow the <strong>cowsay</strong> link above.</p></main></body></html>`;
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(html);
-        res.write(cowsayText);
-
         res.end();
         return undefined;
       }
+      if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/cowsay') {
+        let cowsayText = cowsay.say({ text: 'sup.' });
+        if (parsedRequest.url.query.text) {
+          cowsayText = cowsay.say({ text: parsedRequest.url.query.text });
+        } else {
+          const fakedString = faker.random.words();
+          cowsayText = cowsay.say({ text: fakedString });
+        }
+        const html = `<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/">home</a></li></ul></nav></header><h1> cowsay</h1><pre>${cowsayText}</pre></body></html>`;
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        res.end();
+        return undefined;
+      }
+
       return undefined;
     });
   return undefined;
