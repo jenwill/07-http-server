59cd05ae0a6c097f3b442fc0c930fb272c79c136
Jennifer Williams <jennifer@squareandplum.com>
2018-04-24 20:36:22 -0700

    fix linter errors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const server = require('./lib/server');
+const logger = require('./lib/logger');
 
 // server.start(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
 
-server.start(3000, () => console.log('Listening on port 3000'));
+server.start(3000, () => logger.log(logger.INFO, 'Listening on port 3000'));
